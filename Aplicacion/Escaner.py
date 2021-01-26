4717d3a50bd59d8d66938ed7cae3a1a939522dfb
Gabriel Arispe <45526954+SharkDiamond@users.noreply.github.com>
2021-01-26 11:05:36 -0600

    Update Escaner.py
    Se dividió el código en funciones según las áreas de la interfaz y se terminaron de construir los widget del panel inferior.

--- Aplicacion/Escaner.py
+++ Aplicacion/Escaner.py
@@ -15,6 +15,12 @@ class Aplicacion(PortScanner):
 		self.__Frame4=Frame()
 		self.__Mac=Entry(self.__Frame)
 		self.__Label=Label(self.__Frame)
+		self.__ipFormulario=Entry(self.__Frame4)
+		self.__BotonEscaner=Button(self.__Frame4)
+		self.__textoMostrar=Text(self.__Frame4)
+
+		self.__barra=Scrollbar(self.__Frame4,command=self.__textoMostrar.yview)
+
 		self.__ConstruyeVentana()
 
 
@@ -27,24 +33,54 @@ class Aplicacion(PortScanner):
 		self.__ventana.geometry("700x400")
 		self.__ventana.config(bg="#1D1C24")
 
-		self.__Frame2.pack(side="left")
-		self.__Frame2.config(bg="#292935",width="100",height="400")
+		self.__ConstruyePanelDerecho()
+		self.__ConstruyePanelIzquierdo()
+		self.__ConstruyePanelSuperior()
+		self.__ConstruyePanelInferior()
 
-		self.__Frame3.pack(side="right")
-		self.__Frame3.config(bg="#292935",width="100",height="400")
+		self.__ventana.mainloop()
+
+
+	def __ConstruyePanelSuperior(self):
 
 		self.__Frame.pack(pady=10)
 		self.__Frame.config(bg="#292935",width="400",height="150")
 
+
+		self.__Mac.place(x=150,y=20)
+		self.__Mac.config(text="Mac-Address",bg="#1D1C24",foreground="white")
+		self.__Label.place(x=150,y=60)
+		self.__Label.config(text="Mac-Address")
+
+
+	def __ConstruyePanelInferior(self):
+
 		self.__Frame4.pack(side="bottom",pady=10)
 		self.__Frame4.config(bg="#292935",width="400",height="150")
 
+		self.__ipFormulario.place(x=30,y=40)
+		self.__ipFormulario.config(text="Mac-Address",bg="#1D1C24",foreground="white")
+		self.__BotonEscaner.config(text="Empezar Escaneo",bg="#4D58DA",foreground="white")
+		self.__BotonEscaner.place(x=30,y=80)
+
+		self.__textoMostrar.place(x=165,y=9)
+		self.__textoMostrar.config(bg="#1D1C24",width=28,height=8,foreground="white")
+
+		self.__barra.place(x=375,y=9)
 
 
-		self.__Mac.place(x=150,y=20)
-		self.__Label.place(x=150,y=60)
-		self.__Label.config(text="Mac-Address")
-		self.__ventana.mainloop()
+	def __ConstruyePanelIzquierdo(self):
+
+		self.__Frame2.pack(side="left")
+		self.__Frame2.config(bg="#292935",width="100",height="400")
+
+
+
+	def __ConstruyePanelDerecho(self):
+
+		self.__Frame3.pack(side="right")
+		self.__Frame3.config(bg="#292935",width="100",height="400")
+
 
 
 
